cc82b0aba307945a0d18a677bbc84839effe57ba
Patricia Chun <yejeechun@gmail.com>
2016-07-10 17:53:59 -0400

    Send a hardcoded JSON response.

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.json([
+    {
+      title: 'My hardcoded note.',
+      body_html: 'Wow.'
+    },
+    {
+      title: 'Another hardcoded note.',
+      body_html: 'Such JSON.'
+    }
+  ]);
+
+});
+
+app.listen(3030, function() {
+  console.log('Listening on http://localhost:3030...');
+});
