aadb236900335ba5049f54d2e02cb6bedb2e7936
Patricia Chun <yejeechun@gmail.com>
2016-07-11 14:32:28 -0400

    Create users, ignoring passwords.

---
+++ routes/user-routes.js
@@ -0,0 +1,18 @@
+var router = require('express').Router();
+var User = require('../models/user');
+
+// CREATE a user
+router.post('/', function(req, res) {
+  var user = new User({
+       name: req.body.user.name,
+       username: req.body.user.username,
+  });
+
+  user
+     .save()
+     .then(
+       userData => res.json(userData)
+     );
+});
+
+module.exports = router;
