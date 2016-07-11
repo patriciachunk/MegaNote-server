c036e70fc689a7921178ac2307a37414956b1fa6
Patricia Chun <yejeechun@gmail.com>
2016-07-11 13:14:03 -0400

    Sort notes on the server.

--- app.js
+++ app.js
@@ -5,22 +5,30 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// Middleware
 app.use(function(req, res, next) {
+  // Allow CORS
    res.header('Access-Control-Allow-Origin', '*');
+
+   // Allow Content-Type header (for JSON Payloads)
    res.header('Access-Control-Allow-Headers', 'Content-Type');
    next();
  });
 
+// Body parsing for JSON POST/PUT payloads
 app.use(bodyParser.json());
 
+// READ all notes
 app.get('/', function(req, res) {
 Note
      .find()
+     .sort({ updated_at: 'desc' })
      .then(function(notes) {
        res.json(notes);
      });
 });
 
+// CREATE a note
 app.post('/', function(req, res) {
    var note = new Note({
      title: req.body.note.title,
