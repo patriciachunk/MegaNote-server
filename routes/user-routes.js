fa4dbc3165f9ed37051e7efe99c949fbacdf706a
Patricia Chun <yejeechun@gmail.com>
2016-07-11 14:38:21 -0400

    Respond with a JWT upon signing up.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 var User = require('../models/user');
 
 // CREATE a user
@@ -19,7 +20,15 @@ router.post('/', function(req, res) {
   user
      .save()
      .then(
-       userData => res.json(userData)
+       u userData => {
+         var token = jwt.sign(userData._id, process.env.JWT_SECRET, {
+           expiresIn: 60*60*24
+         });
+         res.json({
+           user: userData,
+           authToken: token
+         });
+       }
      );
 });
 
