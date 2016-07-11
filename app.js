33d7e3de174a50d76ed968ec20934d1a9101e795
Patricia Chun <yejeechun@gmail.com>
2016-07-10 20:40:29 -0400

    Extract DB config to a module.

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+require('dotenv').load();
 var express = require('express');
+var db = require('./config/db');
+
 var app = express();
 
-app.get('/', function(req, res) {
-  res.json([
-    {
-      title: 'My hardcoded note.',
-      body_html: 'Wow.'
-    },
-    {
-      title: 'Another hardcoded note.',
-      body_html: 'Such JSON.'
-    }
-  ]);
+var noteSchema = db.Schema({
+   title: String,
+   body_html: String,
+   body_text: String,
+   updated_at: { type: Date, default: Date.now }
+ });
+ var Note = db.model('Note', noteSchema);
 
+app.get('/', function(req, res) {
+Note
+     .find()
+     .then(function(notes) {
+       res.json(notes);
+     });
 });
 
 app.listen(3030, function() {
