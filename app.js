15e5929dee9808c03cb1d7f37eec12f7a2f3faae
Patricia Chun <yejeechun@gmail.com>
2016-07-11 13:16:15 -0400

    Update notes.

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ var app = express();
 
 // Middleware
 app.use(function(req, res, next) {
-  // Allow CORS
+  // Allow CORS.
    res.header('Access-Control-Allow-Origin', '*');
 
-   // Allow Content-Type header (for JSON Payloads)
+   // Allow Content-Type header (for JSON Payloads).
    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+   // Allow more HTTP verbs.
+   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+
+   // Continue processing the request.
    next();
  });
 
@@ -27,6 +32,17 @@ Note
        res.json(notes);
      });
 });
+
+// READ one note
+ app.get('/:id', function(req, res) {
+   Note
+     .findOne({
+       _id: req.params.id
+     })
+     .then(function(note) {
+       res.json(note);
+     });
+ });
 
 // CREATE a note
 app.post('/', function(req, res) {
@@ -44,6 +60,26 @@ app.post('/', function(req, res) {
        });
      });
  });
+
+ // UPDATE a note
+  app.put('/:id', function(req, res) {
+    Note
+      .findOne({
+        _id: req.params.id
+      })
+      .then(function(note) {
+        note.title = req.body.note.title;
+        note.body_html = req.body.note.body_html;
+        note
+          .save()
+          .then(function() {
+            res.json({
+              message: 'Your changes have been saved.',
+              note: note
+            });
+          });
+      });
+  });
 
 app.listen(3030, function() {
   console.log('Listening on http://localhost:3030...');
